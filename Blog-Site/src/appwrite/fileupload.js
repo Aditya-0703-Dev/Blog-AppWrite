ce092ccd36542aa1e174222635d556afa5c35c16
Aditya <devaditya491@gmail.com>
2024-11-28 15:49:17 +0530

    more CRUD services for articles and images

---
+++ Blog-Site/src/appwrite/fileupload.js
@@ -0,0 +1,55 @@
+import config from '../config/config.js'
+import { Client, ID } from "appwrite";
+
+export class FileUploadService {
+    client = new Client();
+    storage;
+
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteURL)
+            .setProject(config.appwriteProjectID);
+        this.storage = new Storage(this.client);
+    }
+
+    async CreateFile(file){
+        try {
+            return await this.storage.createFile(
+                config.appwriteBucketID,
+                ID.unique(),
+                file,
+            );
+        } catch (error) {
+            console.log("Appwrite service :: CreateFile :: error", error);
+            return false;
+        }
+    }
+
+    async DeleteFile(fileId){
+        try {
+            await this.storage.deleteFile(
+                config.appwriteBucketID,
+                fileId
+            )
+        } catch (error) {
+            console.log("Appwrite service :: DeleteFile :: error", error);
+            return false;
+        }
+    }
+
+    async GetFilePreview(fileId){
+        try {
+            return await this.storage.getFilePreview(
+                config.appwriteBucketID,
+                fileId
+            )
+        } catch (error) {
+            console.log("Appwrite service :: GetFilePreview :: error", error);
+            return false;
+        }
+    }
+}
+
+const fileUploadService = new FileUploadService();
+
+export default fileUploadService
---
+++ Blog-Site/src/appwrite/post.js
@@ -0,0 +1,95 @@
+import config from '../config/config.js'
+import { Client, Databases, Query} from "appwrite";
+
+export class PostService {
+    client = new Client();
+    databases;
+
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteURL)
+            .setProject(config.appwriteProjectID);
+        this.databases = new Databases(this.client);
+    }
+
+    async CreatePost({title, content, image, slug, userId, status}){
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseID,
+                config.appwriteCollectionID,
+                `${userId}-${slug}`,
+                {
+                    title,
+                    content,
+                    image,
+                    userId,
+                    status
+                }
+            );
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async UpdatePost(slug, {title, content, image, userId, status}){
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseID,
+                config.appwriteCollectionID,
+                slug,
+                {
+                    title,
+                    content,
+                    image,
+                    userId,
+                    status
+                }
+            );
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async DeletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseID,
+                config.appwriteCollectionID,
+                slug
+            );
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+
+    async GetPost(slug){
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseID,
+                config.appwriteCollectionID,
+                slug
+            );
+        } catch (error) {
+            console.log("Appwrite serive :: GetPost :: error", error);
+            return false;
+        }
+    }
+
+    async GetPosts(query = Query.select(["status", "active"])){
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseID,
+                config.appwriteCollectionID,
+                query
+            );
+        } catch (error) {
+            console.log("Appwrite serive :: GetPosts :: error", error);
+            return false;
+        }
+    }
+}
+
+const postService = new PostService();
+
+export default postService;
