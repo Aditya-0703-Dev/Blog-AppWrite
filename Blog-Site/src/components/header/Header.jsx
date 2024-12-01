bb1d20c1d42f645bcf45f5e64208b549d5a9f1f8
Aditya <devaditya491@gmail.com>
2024-12-01 16:32:05 +0530

    created some reusable components for the app

--- Blog-Site/src/components/header/Header.jsx
+++ Blog-Site/src/components/header/Header.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import {React} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import {Container, Logo, LogoutBtn} from '../componentsIndex'
 
 function Header() {
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+    const navigate = useNavigate();
+
+    const navItems = [
+        {
+            title: 'Home',
+            slug: '/',
+            active: true
+        },
+        {
+            title: 'Login',
+            slug: '/login',
+            active: !isLoggedIn
+        },
+        {
+            title: 'Register',
+            slug: '/register',
+            active: !isLoggedIn
+        },
+        {
+            title: 'All Posts',
+            slug: '/all-posts',
+            active: isLoggedIn
+        },
+        {
+            title: 'Add Post',
+            slug: '/add-post',
+            active: isLoggedIn
+        }
+    ]
+
     return (
-        <div>Header</div>
+        <header className='py-3 shadow bg-gray-500'>
+            <Container>
+                <nav className='flex'>
+                    <div className='mr-4'>
+                        <Logo />
+                    </div>
+                    <ul className='flex ml-auto'>
+                        {navItems.map((item) => (
+                            item.active ? (
+                                <li key={item.title}>
+                                    <button onClick={() => navigate(item.slug)} className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
+                                        {item.name}
+                                    </button>
+                                </li>
+                            ) : null
+                        ))}
+                        {isLoggedIn && (
+                            <li>
+                                <LogoutBtn />
+                            </li>
+                        )}
+                    </ul>
+                </nav>
+            </Container>
+        </header>
     )
 }
 
